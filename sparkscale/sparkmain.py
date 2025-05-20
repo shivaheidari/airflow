8908bf8d9440e0501368d5e542e52aae58c01558
Shiva Heydari <shivaheydari.es@gmail.com>
2025-05-20 12:01:11 -0600

    indexer

--- sparkscale/sparkmain.py
+++ sparkscale/sparkmain.py
@@ -1,6 +1,8 @@
 #starting a spark session
 
 from pyspark.sql import SparkSession
+from pyspark.ml.feature import VectorAssembler, StringIndexer
+
 
 spark = SparkSession.builder \
     .appName("SparkML-Practice") \
@@ -8,3 +10,15 @@ spark = SparkSession.builder \
 
 df = spark.read.csv("Data/dementia_dataset.csv", header=True, inferSchema=True)
 df.show(5)
+
+indexer = StringIndexer(inputCol="M/F", outputCol="gender_index")
+df = indexer.fit(df).transform(df)
+
+assembler = VectorAssembler(
+    inputCols=["Age", "Visit", "EDUC", "gender_index"], 
+    outputCol="features"
+)
+
+
+
+data = assembler.transform(df).select("features", "Group").show()
