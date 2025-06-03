d524f0a63be619977fe3349163fc9b59a6adbfdd
Shiva Heydari <shivaheydari.es@gmail.com>
2025-06-02 20:20:01 -0600

    params

--- sparkscale/lr_spark.py
+++ sparkscale/lr_spark.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import VectorAssembler, StringIndexer
 from pyspark.ml import Pipeline
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml.classification import LogisticRegression
 
 #spark session
@@ -14,10 +15,18 @@ df = spark.read.csv("Data/dementia_dataset.csv", header=True, inferSchema=True)
 
 print(df.columns)
 categorical_cols = ["M/F", "Hand", "Group"]
-indexers = [StringIndexer(inputCols=col, outputCol=f"{col}_index") for col in categorical_cols]
-assembeler = VectorAssembler(inputCols=["Age"]+[f"{col}_indexe" for col in categorical_cols], outputCol="features")
+indexers = [StringIndexer(inputCol=col, outputCol=f"{col}_index") for col in categorical_cols]
+assembler = VectorAssembler(inputCols=["Age"]+[f"{col}_index" for col in categorical_cols], outputCol="features")
 lr = LogisticRegression(featuresCol="features", labelCol="Group_index")
 
-pipeline = Pipeline(stages=indexers + [assembeler, lr])
+pipeline = Pipeline(stages=indexers + [assembler, lr])
 model = pipeline.fit(df)
 
+predictions = model.transform(df)
+evaluation_acc = MulticlassClassificationEvaluator(labelCol="Group_index", predictionCol="prediction", metricName="accuracy")
+accuracy = evaluation_acc.evaluate(predictions)
+print("Accuracy", accuracy)
+
+evaluation_f1 = MulticlassClassificationEvaluator(labelCol="Group_index", predictionCol="prediction", metricName="f1")
+f1 = evaluation_f1.evaluate(predictions)
+print("f1", f1)
